c5c41c157375750e606d0d5dbce162c5a38aa06b
Ryan <Ryant1234@gmail.com>
2017-05-03 13:55:17 +1200

    what git

---
+++ src/app/WeatherObject.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Ryan Thomas on 03-May-17.
+ */
---
+++ src/app/header.js
@@ -0,0 +1,37 @@
+var React = require('react');
+
+var styles = {
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: '#1f1f1f'
+  },
+  title: {
+    flex: 1,
+    fontSize: '1.5rem',
+    margin: '1rem'
+  },
+  date: {
+    flex: 1,
+    textAlign: 'right',
+    margin: '1rem',
+    color: 'white'
+  }
+};
+
+module.exports = React.createClass({
+  render: function () {
+    return (
+      <header style={styles.header}>
+        <p style={styles.title}>
+          <a href="https://github.com/FountainJS/generator-fountain-webapp" target="_blank" rel="noopener noreferrer">
+            Fountain Generator
+          </a>
+        </p>
+        <p style={styles.date}>
+          Generated with FountainJS v1.0.0 on Tue May 02 2017 11:13:16 GMT+1200 (New Zealand Standard Time)
+        </p>
+      </header>
+    );
+  }
+});
