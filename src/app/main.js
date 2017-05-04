a276f81eed46f85b929c283c01868fd5d3ed0ecb
Ryan <Ryant1234@gmail.com>
2017-05-04 12:19:37 +1200

    stuff

--- src/app/main.js
+++ src/app/main.js
@@ -34,10 +34,19 @@ module.exports = React.createClass({
   }
 });
 
+
 var searchButton = document.getElementById('searchButton');
 searchButton.addEventListener('click', searchWeather);
 var data = data;
+var divCity =  document.getElementById('weatherCity');
 var searchCity = document.getElementById('city');
+var loadingText = document.getElementById('load');
+var weatherBox = document.getElementById('weather');
+var divDescription = document.getElementById('weatherDescription');
+var divWindDirection = document.getElementById('windDirection');
+var divMinTemp = document.getElementById('minTemperature');
+var divMaxTemp = document.getElementById('maxTemperature');
+var divTemp = document.getElementById('weatherTemperature')
 
 function searchWeather() {
   var cityName = searchCity.value;
@@ -64,28 +73,136 @@ function searchWeather() {
    }
  
 
-  
-   
-      
-      var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
-    //  weatherData.temperature = data.main.temp; 
+      var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase(),);
+     weatherData.maxTemp = data.main.temp_max;
+      weatherData.minTemp = data.main.temp_min; 
+       weatherData.temperature = data.main.temp; 
+        weatherData.windSpeed = data.wind.speed; 
+         weatherData.windDirection = data.wind.deg; 
+         updateWeather(weatherData)
       
       console.log(weatherData);
       });
 
+} 
+
+
+
+function updateWeather (weatherData) {
+  divCity.textContent = weatherData.CityName;
+   divDescription.textContent = weatherData.description;
+   divWindDirection.textContent = weatherData.windDirection;
+   divMaxTemp.textContent = weatherData.maxTemp;
+   divMinTemp.textContent = weatherData.minTemp;
+   divTemp.textContent = weatherData.temperature;
 }
+
 
 function Weather(cityName, description) {
   this.CityName = cityName;
   this.description = description;
+  this._maxTemp = '';
+  this._minTemp = '';
   this._temperature = '';
+  this._windSpeed = '';
+  this._windDirection = '';
 }
+
+Object.defineProperty(Weather.prototype, 'maxTemp', {
+  get: function() {
+    return this._maxTemp;
+  },
+  set: function(value){
+    return  this._maxTemp = (value)+ 'C.';
+  }
+});
+
+Object.defineProperty(Weather.prototype, 'minTemp', {
+  get: function() {
+    return this._minTemp;
+  },
+  set: function(value){
+  return   this._minTemp = (value) + 'C.';
+  }
+});
+
+Object.defineProperty(Weather.prototype, 'windSpeed', {
+  get: function() {
+    return this._windSpeed;
+  },
+  set: function(value){
+
+
+    
+   return   this._windSpeed;
+  }
+});
+
+Object.defineProperty(Weather.prototype, 'windDirection', {
+  get: function() {
+    
+    return this._windDirection;
+  },
+  set: function(value){
+
+    
+    
+    return  this._windDirection = getWindDirection(value);
+  }
+});
 
 Object.defineProperty(Weather.prototype, 'temperature', {
   get: function() {
     return this._temperature;
   },
   set: function(value){
-    this._temperature = (value - 273 ).toFixed(2) + 'c.';
+    this._temperature =  (value) + 'C.';
   }
 });
+
+function getWindDirection(windAngle)
+{
+if (windAngle >= 348.75 || windAngle  < 33.75 )
+{
+  return "N"
+}
+
+
+
+if (windAngle >= 33.75 &&  windAngle  < 78.75 )
+{
+  return "NE"
+}
+
+if (windAngle >= 78.75 &&  windAngle  < 123.75 )
+{
+  return "E"
+}
+
+if (windAngle >= 123.75 &&  windAngle  < 168.75 )
+{
+  return "SE"
+}
+
+if (windAngle >= 168.75 &&  windAngle  < 213.75 )
+{
+  return "S"
+}
+
+if (windAngle >= 213.75 &&  windAngle  < 258.75 )
+{
+  return "SW"
+}
+
+if (windAngle >= 258.75 &&  windAngle  < 303.75 )
+{
+  return "W"
+}
+
+if (windAngle >= 303.75 &&  windAngle  < 348.75 )
+{
+  return "NW"
+}
+
+ 
+}
